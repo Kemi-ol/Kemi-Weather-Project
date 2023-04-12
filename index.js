fe720aa21bf9a4fc6c45d1f9157b8e525b09bb5d
Kemi-ol <130588372+Kemi-ol@users.noreply.github.com>
2023-04-12 21:18:05 +0100

    weather project

---
+++ index.js
@@ -0,0 +1,69 @@
+function defaultSearch(city) {
+  let apiKey = "4de6f78efdcff22546b533360a84173a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function displayWeather(response) {
+  document.querySelector("#location").innerHTML = response.data.name;
+  document.querySelector("#feel").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#cloudy").innerHTML = response.data.weather[0].main;
+  document.querySelector("#main-weather-icon").innerHTML = null;
+  {
+    let actualTemp = Math.round(response.data.main.temp);
+    let actualTempElement = document.querySelector("#temp-value");
+    actualTempElement.innerHTML = `${actualTemp}ºC`;
+  }
+  {
+    let humidity = Math.round(response.data.main.humidity);
+    let humidityElement = document.querySelector("#humidityTemp");
+    humidityElement.innerHTML = `${humidity}%`;
+  }
+}
+
+function searchLocation(response) {
+  let apiKey = "4de6f78efdcff22546b533360a84173a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${response.coords.latitude}&lon=${response.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#text-input").value;
+  defaultSearch(city);
+}
+
+defaultSearch("London");
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let searchForm = document.querySelector("#weather-form");
+searchForm.addEventListener("submit", search);
+
+let currentDate = new Date();
+
+let hour = (currentDate.getHours() < 10 ? "0" : "") + currentDate.getHours();
+let minute =
+  (currentDate.getMinutes() < 10 ? "0" : "") + currentDate.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentDate.getDay()];
+let date = document.querySelector("#current-date");
+date.innerHTML = `${day}, ${hour}:${minute}`;
